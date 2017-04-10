d78433551511c69395331f65ff99c7fd9e85f228
Matthew Augsburger <matthewaugsburger@gmail.com>
2017-04-10 15:26:31 -0500

    updated alpha stacker

--- alpha-stacker.js
+++ alpha-stacker.js
@@ -1,9 +1,12 @@
 
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphabet = ["", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 function stackLetters(array) {
     var print = "";
-    for (var i = 0; i < array.length; i++) {
-        print = print + array[i];
+    for (var i = 1; i < array.length; i++) {
+        print += array[i];
+        if (i%3 === 0) {
+            print += " ";
+        }
         console.log(print);
     }
 }
